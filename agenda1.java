b90cb44c8b6460552e13d8674685bebd62e234db
Alumno <alumno@localhost.localdomain>
2014-03-24 08:08:53 -0600

    first commit

---
+++ agenda1.java
@@ -0,0 +1,30 @@
+package arreglos;
+
+import java.util.Scanner;
+
+public class agenda1 {
+
+	public static void main(String[] args) {
+		Scanner arreglo= new Scanner(System.in);
+
+		int arr[] = new  int [5];
+		
+		System.out.println("Ingrese el numero telefonico");
+		arr[0]= arreglo.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[1]= arreglo.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[2]= arreglo.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[3]= arreglo.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[4]= arreglo.nextInt();
+	
+			System.out.println("agenda telefonica ");
+			for (int i=0; i<arr.length; i++){
+				System.out.println(arr[i]);
+			{
+		}
+}		
+}
+}
